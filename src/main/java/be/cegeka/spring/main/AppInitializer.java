9bbacea86908693990c9868e47c1e5f032850a1f
VerachtertAaron <aaron_verachtert@hotmail.com>
2016-06-29 22:51:43 +0200

    @EnableWebMvc met AppInitializer

--- src/main/java/be/cegeka/spring/main/AppInitializer.java
+++ src/main/java/be/cegeka/spring/main/AppInitializer.java
@@ -1,14 +1,28 @@
 package be.cegeka.spring.main;
 
-public class AppInitializer {
-	/*
-	 * @Override public void onStartup(ServletContext servletContext) throws ServletException { System.out.println(
-	 * "Found AppInitializer"); WebApplicationContext context = getContext(); servletContext.addListener(new
-	 * ContextLoaderListener(context)); ServletRegistration.Dynamic dispatcher = servletContext.addServlet(
-	 * "hello-dispatcher", new DispatcherServlet(context)); dispatcher.setLoadOnStartup(1); //
-	 * dispatcher.addMapping("/"); }
-	 * 
-	 * private AnnotationConfigWebApplicationContext getContext() { AnnotationConfigWebApplicationContext context = new
-	 * AnnotationConfigWebApplicationContext(); context.setConfigLocation("be.cegeka.spring.main"); return context; }
-	 */
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+public class AppInitializer implements WebApplicationInitializer {
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        System.out.println("Found AppInitializer");
+        WebApplicationContext context = getContext(SpringRootConfig.class);
+        ServletRegistration.Dynamic dispatcher = servletContext.addServlet("hello-dispatcher", new DispatcherServlet(context));
+        dispatcher.setLoadOnStartup(1);
+        dispatcher.addMapping("/");
+    }
+
+    private AnnotationConfigWebApplicationContext getContext(Class clazz) {
+        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+        context.register(clazz);
+        return context;
+    }
 }
--- src/main/java/be/cegeka/spring/main/SpringRootConfig.java
+++ src/main/java/be/cegeka/spring/main/SpringRootConfig.java
@@ -2,9 +2,11 @@ package be.cegeka.spring.main;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
+@EnableWebMvc
 @ComponentScan(basePackages = "be.cegeka.spring.main")
-public class SpringRootConfig {
+class SpringRootConfig {
 
 }
